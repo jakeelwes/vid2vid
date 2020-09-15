fe4f34dd2a4f637773ae759bfcbe0de7d4987f53
Jake Elwes <me@jakeelwes.com>
2020-09-15 17:23:50 +0100

    change script loc

---
+++ zizi-scripts/train.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+
+# docker run --gpus all --ipc=host -ti --volume=$PWD:/vid2vid --net=host zizi:base-vid2vid bash
+
+folder=${1?Error: no dir given}
+# cf=${2?Error: no close full given}
+cont=${2?Error: cont or start}
+
+if [[ $cont = "cont" ]]
+then
+  conti="--continue_train"
+else
+  conti=""
+fi
+
+echo $folder "training"
+
+docker run --gpus all --shm-size 11G --volume=$PWD/zizi-data:/vid2vid/zizi-data zizi:base-vid2vid \
+    python train.py --name $(basename $folder) \
+    --dataroot $folder --checkpoints_dir zizi-data/checkpoints/ --dataset_mode pose \
+    --input_nc 6 --n_scales_spatial 1 --num_D 2 --ngf 29 --ndf 25 \
+    --resize_or_crop scaleHeight --loadSize 850 --fineSize 850 \
+    --no_first_img --n_frames_total 12 --max_t_step 4 --add_face_disc \
+    --niter_fix_global 3 --niter 5 --niter_decay 5 \
+    --lr 0.0001 --max_frames_per_gpu 1 \
+    --display_freq 100 --tf_log $conti
+
+# 40 700 - 9.6G
+# 30 800 - 10.9G
+# 12 1000 - 10.8G
