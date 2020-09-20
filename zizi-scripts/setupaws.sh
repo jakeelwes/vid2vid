cc4d15bc4fb4cdab62702a432dadebabc445d512
Ubuntu <ubuntu@ip-172-31-28-253.eu-west-1.compute.internal>
2020-09-20 16:24:51 +0000

    aws login noemail

--- zizi-scripts/setupaws.sh
+++ zizi-scripts/setupaws.sh
@@ -21,7 +21,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-aws configure set default.region eu-west-1; aws configure set aws_access_key_id $key ; aws configure set aws_secret_access_key $skey; aws ecr get-login | sudo sh
+aws configure set default.region eu-west-1; aws configure set aws_access_key_id $key ; aws configure set aws_secret_access_key $skey; aws ecr get-login --no-include-email | sudo sh
 
 cd vid2vid
 
