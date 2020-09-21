97956e1cce7713a0163183d9f181829a2399cdfd
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-09-21 12:40:10 +0000

    gg

--- zizi-scripts/setupaws.sh
+++ zizi-scripts/setupaws.sh
@@ -31,6 +31,7 @@ mkdir ./zizi-local/checkpoints
 
 aws s3 sync s3://zizi.data/datasets/looks/$dir ./zizi-local/$dir
 
+(crontab -l 2>/dev/null; echo “*/15 * * * * aws s3 sync /home/jakeelwes/vid2vid/zizi-local/checkpoints/$dir s3://zizi.data/checkpoints/$dir") | crontab -
 
 
 
